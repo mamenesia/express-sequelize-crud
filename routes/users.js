051a9b9d3a4e034bcd74232bbe057de6c694f592
masb0ymas <fajriajjha@gmail.com>
2020-10-19 08:53:38 +0700

    fix: route get all & create user

--- routes/users.js
+++ routes/users.js
@@ -2,57 +2,11 @@ var express = require("express");
 var router = express.Router();
 var model = require("../models/index");
 
+const UserController = require('../controllers/UserController')
+
 /* GET users listing. */
-router.get("/", async function (req, res, next) {
-  try {
-    const users = await model.users.findAll({});
-    if (users.length !== 0) {
-      res.json({
-        status: "OK",
-        messages: "",
-        data: users,
-      });
-    } else {
-      res.json({
-        status: "ERROR",
-        messages: "EMPTY",
-        data: {},
-      });
-    }
-  } catch (err) {
-    res.json({
-      status: "ERROR",
-      messages: "err.message",
-      data: {},
-    });
-  }
-});
-
-router.post("/", async function (req, res, next) {
-  try {
-    const { name, email, gender, phoneNumber } = req.body;
-    const users = await model.users.create({
-      name,
-      email,
-      gender,
-      phone_number: phoneNumber,
-    });
-
-    if (users) {
-      res.status(201).json({
-        status: "OK",
-        messages: "User berhasil ditambahkan",
-        data: users,
-      });
-    }
-  } catch (err) {
-    res.status(400).json({
-      status: "ERROR",
-      messages: err.message,
-      data: {},
-    });
-  }
-});
+router.get("/", UserController.getAll);
+router.post("/", UserController.create);
 
 router.patch("/:id", async function (req, res, next) {
   try {
